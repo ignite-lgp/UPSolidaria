80d584b54fd5bec7e7761eb502a2ee30eabf441f
Cowtard <ei12109@fe.up.pt>
2016-05-21 17:08:33 +0100

    Temp 404 page.

--- resources/views/organizacao.blade.php
+++ resources/views/organizacao.blade.php
@@ -168,7 +168,7 @@
 	<section class="container" id="main-container-xs">
 				<section class="organization-section visible-xs">
 					<h3 class="title">{{ $info->name}}</h3>
-					<img class="img img-responsive org-img" src="/src/imgs/gas_porto.png">
+					<img class="img img-responsive org-img" src="/src/	imgs/gas_porto.png">
 					<section class="info-section">
 							<h4 class="sidebar-title">Missão</h4>
 							<p class="big">{{ $info->mission }}</p>
